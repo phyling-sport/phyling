5f8cb160d358b66212f0f5cf4690fd49e3942135
Stagiaire 1 <stagiaire1@MacBook-Pro-de-Stagiaire.local>
2026-02-16 15:56:08 +0100

    fix: rolling median min

--- phyling/ble/nanophyling.py
+++ phyling/ble/nanophyling.py
@@ -311,7 +311,7 @@ class NanoPhyling:
 
         if rolling:
             rate = self.config.get("rate")
-            window = window_s * rate
+            window = max(1, round(window_s * rate))
 
             cols_to_smooth = ["acc_x", "acc_y", "acc_z", "gyro_x", "gyro_y", "gyro_z"]
 
